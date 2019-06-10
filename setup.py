0d187e148510298e7a005db93e8c02f8974e9523
Paul van Gool <pvangool@gmail.com>
2019-06-10 07:50:21 -0700

    LEt's try it this way.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rcs_chatbot",
-    version="0.0.1",
+    version="0.0.2",
     author="Paul van Gool",
     author_email="pvangool@gmail.com",
     description="Python SDK for RCS MaaP chatbot",
